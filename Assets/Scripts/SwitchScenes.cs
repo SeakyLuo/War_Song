9f9a4f3f79f357a114974b2baefca7102b5da801
haitianluo@umail.ucsb.edu <luohaitian@169-231-157-6.wireless.ucsb.edu>
2018-04-10 15:57:02 -0700

    Main Scenes Created

---
+++ Assets/Scripts/SwitchScenes.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SwitchScenes : MonoBehaviour {
+
+    public void EnterCollection()
+    {
+        SceneManager.LoadScene("Collections");
+    }
+
+    public void EnterWar()
+    {
+        SceneManager.LoadScene("PlayerMatching");
+    }
+
+    public void EnterRecruitment()
+    {
+        SceneManager.LoadScene("Recruitment");
+    }
+}
