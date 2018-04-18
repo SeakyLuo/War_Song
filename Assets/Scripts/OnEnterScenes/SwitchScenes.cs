bb76b8b111f9d67144b80f40dc22af742bf1992c
haitianluo@umail.ucsb.edu <luohaitian@Seakys-MacBook-Pro.local>
2018-04-17 19:08:08 -0700

    Time Added

--- Assets/Scripts/OnEnterScenes/SwitchScenes.cs
+++ Assets/Scripts/OnEnterScenes/SwitchScenes.cs
@@ -9,7 +9,6 @@ using System;
 public class SwitchScenes : MonoBehaviour, IPointerClickHandler
 {
     public GameObject playerInfoPanel, settingsPanel;
-    public Text time;
 
     private Canvas parentCanvas;
     private GameObject[] closeObjects;
@@ -18,11 +17,6 @@ public class SwitchScenes : MonoBehaviour, IPointerClickHandler
     {
         parentCanvas = gameObject.GetComponent<Canvas>();
         closeObjects = new GameObject[] { playerInfoPanel, settingsPanel };
-    }
-
-    private void FixedUpdate()
-    {
-        time.text = DateTime.Now.ToString("h:mm tt");
     }
 
     public void EnterCollection()
---
+++ Assets/Scripts/UpdateTime.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public class UpdateTime : MonoBehaviour {
+
+    public Text time;
+
+    private void FixedUpdate()
+    {
+        time.text = DateTime.Now.ToString("h:mm tt");
+    }
+}
